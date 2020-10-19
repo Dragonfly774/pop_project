b7c5c15c9e853126f79de63d4417ba6faa1c3ef3
Dragonfly774 <romanov7semen@ya.ru>
2020-10-19 16:18:54 +0300

    pop

--- homework1.py
+++ homework1.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QListWidgetItem
 from PyQt5.QtWidgets import QLabel, QLineEdit, QMainWindow, QListWidget
 from bookname import Ui_MainWindow
 
+
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
@@ -11,12 +12,6 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         # uic.loadUi('bookname.ui', self)  # Загружаем дизайн
         self.pushButton.clicked.connect(self.run)
         # Обратите внимание: имя элемента такое же как в QTDesigner
-
-    def run(self):
-        name = self.lineEdit.text()
-        tel = self.lineEdit_2.text()
-        item = (name, tel)
-        self.listWidget.addItem(name + ' ' + tel)
 
 
 if __name__ == '__main__':
